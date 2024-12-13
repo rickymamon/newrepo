17183a731b24ac91d8404fad9d78945bbbc1d7e1
ricky mamon <rickymamon95@gmail.com>
2024-12-14 03:20:00 +0800

    client

--- hop/tb.py
+++ hop/tb.py
@@ -2,3 +2,8 @@ import pytest
 from unittest.mock import patch
 from datetime import datetime
 from app import app, Client
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
