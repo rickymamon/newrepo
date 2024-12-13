63cbe66a1351cfa6b121e1006949df69f3d2ef33
ricky mamon <rickymamon95@gmail.com>
2024-12-14 03:22:36 +0800

    test client

--- hop/tb.py
+++ hop/tb.py
@@ -7,3 +7,24 @@ from app import app, Client
 def client():
     with app.test_client() as client:
         yield client
+
+def test_get_clients(client):
+    # Mocked clients
+    mock_clients = [
+        Client(
+            id=1,
+            first_name="John",
+            last_name="Doe",
+            address="Rizal",
+            contact="098372764",
+            email="ricky@gmail.com"
+        ),
+        Client(
+            id=2,
+            first_name="Ricky",
+            last_name="Smith",
+            address="Pal",
+            contact="09837277834",
+            email="ricky16@gmail.com"
+        ),
+    ]
